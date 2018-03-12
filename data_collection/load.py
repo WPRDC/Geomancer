c69df007d811b87487258de3f466345ec3d690d6
saylorsd <steven.saylor@pitt.edu>
2018-03-12 16:37:10 -0400

    fixed load script

--- data_collection/load.py
+++ data_collection/load.py
@@ -1,11 +1,11 @@
 from django.contrib.gis.utils import LayerMapping
-from geoservices.models import *
+from geostuff.models import *
 import subprocess
 
 from .collection_settings import MODEL_MAPPING, ZIP_MAPPING
 
-# SOURCE_DIR = "/var/www/wprdc_tools/geostuff/source_data/"  # TODO: make relative
-SOURCE_DIR = "/home/sds25/projects/wprdc-tools/geoservices/source_data/"  # TODO: make relative
+SOURCE_DIR = "/var/www/wprdc_tools/geostuff/source_data/"  # TODO: make relative
+#SOURCE_DIR = "/home/sds25/projects/wprdc-tools/geoservices/source_data/"  # TODO: make relative
 
 
 # Define how to map source data files to the models.
@@ -21,17 +21,18 @@ def add_address_city(zips=ZIP_MAPPING):
 
 
 def run(update_parcel=False, update_addr=False, verbose=True, mapping=MODEL_MAPPING):
-    subprocess.run(['psql', 'django', '-c', 'TRUNCATE geoservices_adminregion cascade'])
 
+    subprocess.run(['psql', 'django', '-c', 'TRUNCATE geostuff_adminregion cascade'])
+	
     # if updating parcel, first truncate its table, otherwise remove it from mapping so it's not updated
     if update_parcel:
-        subprocess.run(['psql', 'django', '-c', 'TRUNCATE geoservices_parcel'])
+        subprocess.run(['psql', 'django', '-c', 'TRUNCATE geostuff_parcel'])
     else:
         del (mapping['parcel'])
 
     # if updating address, first truncate its table, otherwise remove it from mapping so it's not updated
     if update_addr:
-        subprocess.run(['psql', 'django', '-c', 'TRUNCATE geoservices_addresspoint'])
+        subprocess.run(['psql', 'django', '-c', 'TRUNCATE geostuff_addresspoint'])
     else:
         del (mapping['address'])
 
